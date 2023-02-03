36bd035ce66fff4ddf4e34346e77e1229431f6cf
Анастасия Боговид <anastaciabogovid@yandex.ru>
2023-02-03 15:44:31 +0300

    Circus

--- Circus1/Program.cs
+++ Circus1/Program.cs
@@ -1,10 +1,50 @@
-﻿namespace Circus1
+﻿namespace Classes;
+
+public class Program
 {
-    internal class Program
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
+        List<ICar> cars = new List<ICar>();
+
+        cars.Add(new DodgeViper());
+        cars.Add(new ShelbyS500());
+
+        foreach (var car in cars)
         {
-            Console.WriteLine("Hello, World!");
+            car.Start();
+        }
+
+        foreach (var car in cars)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                car.ChangeGear();
+            }
+        }
+
+        foreach (var car in cars)
+        {
+            Console.WriteLine(car.CurrentSpeed);
+        }
+
+        foreach (var car in cars)
+        {
+            car.Stop();
+        }
+public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Representator representator = new Representator();
+            //HrSquad hr = new HrSquad(10);
+            //Circus circus = new Circus(representator, hr.HiredActors());
+            //circus.StartShow();
+
+            var a = 1;
+
+            Console.WriteLine(++a);
         }
     }
+
+
 }
